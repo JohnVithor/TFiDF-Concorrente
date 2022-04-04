2b7c3f551683461b72b8352f3eea5a9fc29036ab
johnvithor <jv.venceslau.c@gmail.com>
2022-04-04 10:22:40 -0300

    Adiciona o código serial utilizando o .parquet para salvar os resultados

---
+++ src/main/java/main/Document.java
@@ -0,0 +1,55 @@
+package main;
+
+import java.util.*;
+
+public class Document {
+	static Set<String> stopwords = new HashSet<>();
+	static List<String> id_token_vocabulary = new ArrayList<>();
+	static HashMap<String, Integer> token_id_vocabulary = new HashMap<>();
+	static int vocab_size = 0;
+	private final String title;
+	private final Map<Integer, Double> frequency_table = new HashMap<>();
+	private final double numberOfTerms;
+
+	public Document(String title, String content) {
+		this.title = title;
+		String[] tokens = content
+				.replaceAll("[^a-zA-Z0-9 ]", "")
+				.toLowerCase()
+				.split("\\s+");
+		numberOfTerms = tokens.length;
+		for (String token: tokens) {
+			if(token.length() == 0 || stopwords.contains(token))
+			{
+				continue;
+			}
+			int this_id = token_id_vocabulary.getOrDefault(token, vocab_size);
+
+			if (this_id == vocab_size) {
+				token_id_vocabulary.put(token, vocab_size);
+				id_token_vocabulary.add(token);
+				++vocab_size;
+			}
+
+			frequency_table.compute(this_id, (key, val)
+					-> (val == null)
+					? 1
+					: val + 1);
+		}
+		for (Map.Entry<Integer, Double> set: frequency_table.entrySet()) {
+			frequency_table.put(set.getKey(), set.getValue() / numberOfTerms);
+		}
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Map<Integer, Double> getFrequency_table() {
+		return frequency_table;
+	}
+
+	public double calculateTermFrequency(Integer term_id) {
+		return frequency_table.getOrDefault(term_id,0.0);
+	}
+}
---
+++ src/main/java/main/Main.java
@@ -0,0 +1,112 @@
+package main;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.parquet.avro.AvroParquetWriter;
+import org.apache.parquet.hadoop.ParquetFileWriter;
+import org.apache.parquet.hadoop.ParquetWriter;
+import org.apache.parquet.hadoop.metadata.CompressionCodecName;
+
+public class Main {
+    public static void main(String[] args) {
+        String filename = "devel_100_000";
+
+        List<Document> documentList = new ArrayList<>();
+
+        String input_path = "amazon_review_polarity_csv/"+filename+".csv";
+        String docs_schema_path = "src/main/resources/docs_schema.avsc";
+        String tfidf_schema_path = "src/main/resources/tfidf_schema.avsc";
+        String docs_out_fileName = filename + "_docs_results.parquet";
+        String tfidf_out_fileName = filename+ "_tfidf_results.parquet";
+
+        try(BufferedReader reader = new BufferedReader(new FileReader("amazon_review_polarity_csv/stopwords.txt")))
+        {
+            Document.stopwords.addAll(Arrays.stream(reader.readLine().split(",")).toList());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Schema schema_docs = new Schema.Parser().parse(new FileInputStream(docs_schema_path));
+            Schema schema_tfidf = new Schema.Parser().parse(new FileInputStream(tfidf_schema_path));
+            BufferedReader br = Files.newBufferedReader(Paths.get(input_path), StandardCharsets.UTF_8);
+
+            for (String line; (line = br.readLine()) != null;) {
+                String [] cells = line.split("\",\"");
+                Document d = new Document(cells[1], cells[2]);
+                documentList.add(d);
+            }
+            br.close();
+
+            double[] terms_count_all_docs = new double[Document.vocab_size];
+            System.out.println("Vocabulary Size: " + Document.vocab_size);
+            int doc_len = documentList.size();
+
+            try (ParquetWriter<GenericData.Record> writer = AvroParquetWriter.
+                    <GenericData.Record>builder(new Path(docs_out_fileName))
+                    .withRowGroupSize(ParquetWriter.DEFAULT_BLOCK_SIZE)
+                    .withPageSize(ParquetWriter.DEFAULT_PAGE_SIZE)
+                    .withSchema(schema_docs)
+                    .withConf(new Configuration())
+                    .withCompressionCodec(CompressionCodecName.SNAPPY)
+                    .withValidation(false)
+                    .withDictionaryEncoding(false)
+                    .withWriteMode(ParquetFileWriter.Mode.OVERWRITE)
+                    .build()){
+                GenericData.Record record = new GenericData.Record(schema_docs);
+                for (int i = 0; i < doc_len; ++i) {
+                    for (Map.Entry<Integer, Double> set: documentList.get(i).getFrequency_table().entrySet()) {
+                        terms_count_all_docs[set.getKey()] += 1;
+                    }
+                    record.put("doc_id", i);
+                    record.put("value", documentList.get(i).getTitle());
+                    writer.write(record);
+                }
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+
+            try (ParquetWriter<GenericData.Record> writer = AvroParquetWriter.
+                    <GenericData.Record>builder(new Path(tfidf_out_fileName))
+                    .withRowGroupSize(ParquetWriter.DEFAULT_BLOCK_SIZE)
+                    .withPageSize(ParquetWriter.DEFAULT_PAGE_SIZE)
+                    .withSchema(schema_tfidf)
+                    .withConf(new Configuration())
+                    .withCompressionCodec(CompressionCodecName.SNAPPY)
+                    .withValidation(false)
+                    .withDictionaryEncoding(false)
+                    .withWriteMode(ParquetFileWriter.Mode.OVERWRITE)
+                    .build()) {
+                GenericData.Record record = new GenericData.Record(schema_tfidf);
+                for (int j = 0; j < doc_len; ++j) {
+                    for (int i = 0; i < Document.vocab_size; ++i) {
+                        double idf = Math.log(doc_len / terms_count_all_docs[i]);
+                        double tf = documentList.get(j).calculateTermFrequency(i);
+                        double tfidf = tf*idf;
+                        if (tfidf != 0.0) {
+                            record.put("term", Document.id_token_vocabulary.get(i));
+                            record.put("doc_id", j);
+                            record.put("value", tfidf);
+                            writer.write(record);
+                        }
+                    }
+                    documentList.set(j, null);
+                }
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
