e70d9bfef98391c48f1f77ec03544aabe40029f9
JohnVithor <jv.venceslau.c@gmail.com>
2022-06-08 10:08:56 -0300

    corrigindo tempos obtidos

--- concurrent.sh
+++
@@ -1,2 +0,0 @@
-/home/johnvithor/UFRN/Concorrente/openjdk-19-loom+6-625_linux-x64_bin/jdk-19/bin/java --enable-preview -Dfile.encoding=UTF-8 -classpath /home/johnvithor/UFRN/Concorrente/TFiDF-Loom/target/classes:/home/johnvithor/.m2/repository/org/apache/commons/commons-lang3/3.12.0/commons-lang3-3.12.0.jar tfidf.Concurrent
-
