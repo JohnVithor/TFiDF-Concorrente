27256cb9932cc2982c59950ba8dcb084dee15066
johnvithor <jv.venceslau.c@gmail.com>
2022-06-09 15:45:26 -0300

    refazendo testes de buffer com o jcstress

---
+++ src/main/java/jv/jcstress/BufferStress.java
@@ -0,0 +1,17 @@
+package jv.jcstress;
+
+import org.openjdk.jcstress.annotations.*;
+
+@JCStressTest
+@Description("Teste de buffer")
+@Outcome(id="D, D", expect = Expect.ACCEPTABLE, desc = "")
+public class BufferStress {
+    @Actor
+    public void actor1(BufferState state) {
+    }
+
+    @Actor
+    public void actor2(BufferState state) {
+
+    }
+}
