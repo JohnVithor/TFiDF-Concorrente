99fe1ff74c947919cd341a0479210d433b278837
JohnVithor <jv.venceslau.c@gmail.com>
2022-06-13 09:28:01 -0300

    resultados para versão serial no novo ambiente

--- src/main/java/jv/microbenchmark/Microbenchmark.java
+++ src/main/java/jv/microbenchmark/Microbenchmark.java
@@ -1,8 +1,10 @@
 package jv.microbenchmark;
 
 import jv.microbenchmark.runner.executor.ExecutorConcurrentRunner;
+import jv.microbenchmark.runner.naive.NaiveSerialRunner;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.profile.GCProfiler;
+import org.openjdk.jmh.profile.StackProfiler;
 import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
@@ -11,21 +13,21 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 public class Microbenchmark {
     public static void main(String[] args) throws Exception {
         Options opt = new OptionsBuilder()
-//                .include(NaiveSerialRunner.class.getSimpleName())
+                .include(NaiveSerialRunner.class.getSimpleName())
 //                .include(ThreadConcurrentRunner.class.getSimpleName())
 //                .include(StreamSerialRunner.class.getSimpleName())
 //                .include(StreamConcurrentRunner.class.getSimpleName())
-                .include(ExecutorConcurrentRunner.class.getSimpleName())
+//                .include(ExecutorConcurrentRunner.class.getSimpleName())
                 .shouldDoGC(true)
                 .mode(Mode.Throughput)
                 .addProfiler(GCProfiler.class)
-//                .addProfiler(StackProfiler.class)
+                .addProfiler(StackProfiler.class)
                 .warmupIterations(5)
                 .measurementIterations(10)
                 .forks(1)
                 .jvmArgs("-server"
-                        , "-Xms1024m"
-                        , "-Xmx1024m"
+                        , "-Xms2048m"
+                        , "-Xmx2048m"
 //                        ,"-XX:+UseSerialGC"
 //                        ,"-XX:+UseParallelGC"
 //                        ,"-XX:+UseConcMarkSweepGC"
