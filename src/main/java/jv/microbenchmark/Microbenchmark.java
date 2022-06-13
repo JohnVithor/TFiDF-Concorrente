3ff4ff676b52580606b118199d077b50d079b243
JohnVithor <jv.venceslau.c@gmail.com>
2022-06-13 09:55:40 -0300

    organizando resultados

--- src/main/java/jv/microbenchmark/Microbenchmark.java
+++ src/main/java/jv/microbenchmark/Microbenchmark.java
@@ -2,6 +2,7 @@ package jv.microbenchmark;
 
 import jv.microbenchmark.runner.executor.ExecutorConcurrentRunner;
 import jv.microbenchmark.runner.naive.NaiveSerialRunner;
+import jv.microbenchmark.runner.naive.ThreadConcurrentRunner;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.profile.GCProfiler;
 import org.openjdk.jmh.profile.StackProfiler;
@@ -13,8 +14,8 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 public class Microbenchmark {
     public static void main(String[] args) throws Exception {
         Options opt = new OptionsBuilder()
-                .include(NaiveSerialRunner.class.getSimpleName())
-//                .include(ThreadConcurrentRunner.class.getSimpleName())
+//                .include(NaiveSerialRunner.class.getSimpleName())
+                .include(ThreadConcurrentRunner.class.getSimpleName())
 //                .include(StreamSerialRunner.class.getSimpleName())
 //                .include(StreamConcurrentRunner.class.getSimpleName())
 //                .include(ExecutorConcurrentRunner.class.getSimpleName())
