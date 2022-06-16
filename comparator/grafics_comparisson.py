1fbb2c372fc07b9e22cf2991a7df3dd6b30a340d
johnvithor <jv.venceslau.c@gmail.com>
2022-06-16 15:04:17 -0300

    atualizando visualização dos resultados

---
+++ comparator/grafics_comparisson.py
@@ -0,0 +1,92 @@
+# grafics.py
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sg1gc = pd.read_csv("./Throughput/G1_Serial_Results.csv")
+sg1gc["GC"] = "G1GC"
+szgc = pd.read_csv("./Throughput/Z_Serial_Results.csv")
+szgc["GC"] = "ZGC"
+sshenandoahgc = pd.read_csv("./Throughput/Shenandoah_Serial_Results.csv")
+sshenandoahgc["GC"] = "ShenandoahGC"
+
+cg1gc = pd.read_csv("./Throughput/G1_Concurrent_Results.csv")
+cg1gc["GC"] = "G1GC"
+czgc = pd.read_csv("./Throughput/Z_Concurrent_Results.csv")
+czgc["GC"] = "ZGC"
+cshenandoahgc = pd.read_csv("./Throughput/Shenandoah_Concurrent_Results.csv")
+cshenandoahgc["GC"] = "ShenandoahGC"
+
+dados = pd.concat([sg1gc, szgc, sshenandoahgc, cg1gc, czgc, cshenandoahgc])
+dados.reset_index(inplace=True, drop=True)
+
+dados.Score = dados.Score.str.replace(',', '.').astype(float)
+dados.Benchmark = dados.Benchmark.str.removeprefix("jv.microbenchmark.runner.")
+dados["Class"] = dados.Benchmark.str.split(".").str[1]
+dados["Benchmark"] = dados.Benchmark.str.split("Runner.").str[1]
+dados["Threads"] = dados["Param: n_threads"].astype(int)
+dados.dropna(subset = ['Score'], inplace=True)
+dados.drop(["Param: dataset",
+            "Param: stringManipulation",
+            "Param: n_threads",
+            "Param: buffer_size",
+            "Samples", "Mode",
+            "Score Error (99,9%)","Unit"],
+            axis=1, inplace=True)
+
+serial = dados[dados.Class == "NaiveSerialRunner"]
+serial2 = serial.copy()
+serial2["Threads"] = 2
+serial4 = serial.copy()
+serial4["Threads"] = 4
+serial6 = serial.copy()
+serial6["Threads"] = 6
+serial8 = serial.copy()
+serial8["Threads"] = 8
+serial12 = serial.copy()
+serial12["Threads"] = 12
+
+data = pd.concat([dados, serial2, serial4, serial6, serial8, serial12])
+data.reset_index(inplace=True, drop=True)
+
+alvos=set(["compute_df", "compute_tfidf"])
+plt.figure(figsize=(10,5))
+g = sns.relplot(x="Threads", y="Score", col="Benchmark",
+                hue="Class", style="GC", kind="line",
+                ci="sd", palette="dark", alpha=.6,
+                data=data[data["Benchmark"].isin(alvos)])
+g.savefig("imgs/Throughput per Thread.png")
+
+alvos=set(["compute_df:·gc.alloc.rate", "compute_tfidf:·gc.alloc.rate"])
+plt.figure(figsize=(10,5))
+g = sns.relplot(x="Threads", y="Score", col="Benchmark",
+                hue="Class", style="GC", kind="line",
+                ci="sd", palette="dark", alpha=.6,
+                data=data[data["Benchmark"].isin(alvos)])
+g.savefig("imgs/GC allocation rate per Thread.png")
+
+dataC = data[data.Class != "NaiveSerialRunner"]
+alvos=set(["compute_df:·gc.alloc.rate", "compute_tfidf:·gc.alloc.rate"])
+plt.figure(figsize=(10,5))
+g = sns.relplot(x="Threads", y="Score", col="Benchmark",
+                hue="Class", style="GC", kind="line",
+                ci="sd", palette="dark", alpha=.6,
+                data=dataC[dataC["Benchmark"].isin(alvos)])
+g.savefig("imgs/Concurrent GC allocation rate per Thread.png")
+
+alvos=set(["compute_df:·gc.time", "compute_tfidf:·gc.time"])
+plt.figure(figsize=(10,5))
+g = sns.relplot(x="Threads", y="Score", col="Benchmark",
+                hue="Class", style="GC", kind="line",
+                ci="sd", palette="dark", alpha=.6,
+                data=data[data["Benchmark"].isin(alvos)])
+g.savefig("imgs/GC time spent per Thread.png")
+
+alvos=set(["compute_df:·gc.count", "compute_tfidf:·gc.count"])
+plt.figure(figsize=(10,5))
+g = sns.relplot(x="Threads", y="Score", col="Benchmark",
+                hue="Class", style="GC", kind="line",
+                ci="sd", palette="dark", alpha=.6,
+                data=data[data["Benchmark"].isin(alvos)])
+g.savefig("imgs/GC counts per Thread.png")
--- comparator/grafics_concurrent_executor.py
+++ comparator/grafics_concurrent_executor.py
@@ -3,38 +3,39 @@
 import pandas as pd
 import seaborn as sns
 
-concurrent = pd.read_csv("./Throughput/ConcurrentResults.csv")
-concurrent["Class"] = "Raw Threads"
-executor = pd.read_csv("./Throughput/ExecutorResults.csv")
-executor["Class"] = "Executor"
+g1gc = pd.read_csv("./Throughput/G1_Concurrent_Results.csv")
+g1gc["GC"] = "G1GC"
+zgc = pd.read_csv("./Throughput/Z_Concurrent_Results.csv")
+zgc["GC"] = "ZGC"
+shenandoahgc = pd.read_csv("./Throughput/Shenandoah_Concurrent_Results.csv")
+shenandoahgc["GC"] = "ShenandoahGC"
 
-dados = pd.concat([concurrent, executor])
+dados = pd.concat([g1gc, zgc, shenandoahgc])
 
 dados.Score = dados.Score.str.replace(',', '.').astype(float)
-dados.Benchmark = dados.Benchmark.str.removeprefix("jv.microbenchmark.runners.runner.")
+dados.Benchmark = dados.Benchmark.str.removeprefix("jv.microbenchmark.runner.")
+dados["Class"] = dados.Benchmark.str.split(".").str[1]
 dados["Benchmark"] = dados.Benchmark.str.split("Runner.").str[1]
-dados.dropna(subset = ['Score'], inplace=True) 
-replace_string = {"foreach_java":"Java", "foreach_apache":"Apache"}
-dados["String"] = dados["Param: stringManipulation"].replace(replace_string)
-dados.drop(["Param: dataset", "Param: stringManipulation"], axis=1, inplace=True)
+dados["Threads"] = dados["Param: n_threads"].astype(int)
+dados.dropna(subset = ['Score'], inplace=True)
+dados.drop(["Param: dataset",
+            "Param: stringManipulation",
+            "Param: n_threads"], axis=1, inplace=True)
 alvos=set(["compute_df", "compute_tfidf"])
-
-# Apens versão com Apache
-dados = dados[dados["String"] == "Apache"]
 
 g = sns.catplot(
     data=dados[dados["Benchmark"].isin(alvos)], kind="bar",
-    col="Benchmark",
-    x="String", y="Score", hue="Class",
+    col="Benchmark", row="Class",
+    x="GC", y="Score", hue="Threads",
     ci="sd", palette="dark", alpha=.6, height=6, sharey=True,
 )
-g.savefig("imgs/Concurrent X Executor (Throughput).png")
+g.savefig("imgs/Concurrent GCs (Throughput).png")
 
 alvos=set(["compute_df:·gc.alloc.rate.norm", "compute_tfidf:·gc.alloc.rate.norm"])
 g = sns.catplot(
     data=dados[dados["Benchmark"].isin(alvos)], kind="bar",
-    col="Benchmark",
-    x="String", y="Score", hue="Class",
+    col="Benchmark", row="Class",
+    x="GC", y="Score", hue="Threads",
     ci="sd", palette="dark", alpha=.6, height=6, sharey=True,
 )
-g.savefig("imgs/Concurrent X Executor (GC).png")
+g.savefig("imgs/Concurrent GCs (GC).png")
