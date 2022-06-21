5e37d80cd21c5f2e15a9b7443a348bb215f76862
JohnVithor <jv.venceslau.c@gmail.com>
2022-06-21 11:53:38 -0300

    adicionando resultados de forkjoin

--- src/main/java/jv/microbenchmark/Microbenchmark.java
+++ src/main/java/jv/microbenchmark/Microbenchmark.java
@@ -2,6 +2,7 @@ package jv.microbenchmark;
 
 import jv.microbenchmark.runner.atomic.AtomicConcurrentRunner;
 import jv.microbenchmark.runner.executor.ExecutorConcurrentRunner;
+import jv.microbenchmark.runner.forkjoin.ForkJoinRunner;
 import jv.microbenchmark.runner.naive.NaiveSerialRunner;
 import jv.microbenchmark.runner.naive.ThreadConcurrentRunner;
 import org.openjdk.jmh.annotations.Mode;
@@ -15,12 +16,13 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 public class Microbenchmark {
     public static void main(String[] args) throws Exception {
         Options opt = new OptionsBuilder()
-                .include(NaiveSerialRunner.class.getSimpleName())
+//                .include(NaiveSerialRunner.class.getSimpleName())
 //                .include(ThreadConcurrentRunner.class.getSimpleName())
 //                .include(StreamSerialRunner.class.getSimpleName())
 //                .include(StreamConcurrentRunner.class.getSimpleName())
 //                .include(ExecutorConcurrentRunner.class.getSimpleName())
 //                .include(AtomicConcurrentRunner.class.getSimpleName())
+                .include(ForkJoinRunner.class.getSimpleName())
                 .shouldDoGC(true)
                 .mode(Mode.Throughput)
                 .addProfiler(GCProfiler.class)
@@ -36,11 +38,11 @@ public class Microbenchmark {
 //                        ,"-XX:+UseParallelGC"
 //                        ,"-XX:+UseConcMarkSweepGC"
 //                        ,"-XX:+UseStringDeduplication"
-//                        ,"-XX:+UseG1GC"
+                        ,"-XX:+UseG1GC"
 //                        ,"-XX:+UseZGC"
 //                        ,"-XX:+UseShenandoahGC"
                 )
-                .result("Sresults.csv")
+                .result("results.csv")
                 .resultFormat(ResultFormatType.CSV)
                 .build();
         new Runner(opt).run();
