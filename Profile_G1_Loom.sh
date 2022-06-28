989510fd988868df8ca7b1f2e94fceb68fec1457
JohnVithor <jv.venceslau.c@gmail.com>
2022-06-28 14:52:27 -0300

    atualizando resultados da versão com o Loom

---
+++ Profile_G1_Loom.sh
@@ -0,0 +1,3 @@
+/home/johnvithor/UFRN/Concorrente/openjdk-19-loom+6-625_linux-x64_bin/jdk-19/bin/java -Xms2g -Xmx2g -XX:MaxMetaspaceSize=512m -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC --enable-preview -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -XX:+FlightRecorder -XX:StartFlightRecording=name="G1_Loom_Profiler",settings="profile",dumponexit=true,filename="./G1_Loom_Profiler.jfr" -XX:FlightRecorderOptions=stackdepth=2048 -Dfile.encoding=UTF-8 -classpath /home/johnvithor/UFRN/Concorrente/TFiDF-Concorrente/target/classes:/home/johnvithor/.m2/repository/org/apache/commons/commons-lang3/3.12.0/commons-lang3-3.12.0.jar tfidf.Concurrent
+
+
--- profile_loom.sh
+++
@@ -1,3 +0,0 @@
-/home/johnvithor/UFRN/Concorrente/openjdk-19-loom+6-625_linux-x64_bin/jdk-19/bin/java -Xms2g -Xmx2g -XX:MaxMetaspaceSize=512m -XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC --enable-preview -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -XX:+FlightRecorder -XX:StartFlightRecording=name="Shenandoah_Loom_Profiler",settings="profile",dumponexit=true,filename="./Shenandoah_Loom_Profiler.jfr" -XX:FlightRecorderOptions=stackdepth=2048 -Dfile.encoding=UTF-8 -classpath /home/johnvithor/UFRN/Concorrente/TFiDF-Concorrente/target/classes:/home/johnvithor/.m2/repository/org/apache/commons/commons-lang3/3.12.0/commons-lang3-3.12.0.jar tfidf.Concurrent
-
-
